41c64e4858786a775682c81f0b5953867250f7f8
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 17:19:45 +0500

    Create problem20.cpp

---
+++ problem20.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << ((a >= 0) || (b >= 0) || (c >= 0) || (d >= 0) || (e >= 0)) << endl;
+    return 0;
+}
