62fe8613343fcfe82b5613466534ae39fc503b01
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 16:54:41 +0500

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 189;
+    char b = 100;
+    float x = a / 3.0;
+    int y = b;
+    float z = (a + b) / 5.0;
+    float w = b + (a - 1);
+    cout << x << " " << y << " " << z << " " << w << endl;
+    return 0;
+}
