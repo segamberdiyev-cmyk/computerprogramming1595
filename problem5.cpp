71e860b1bf324b6650533f9656a49116383c6988
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 16:55:41 +0500

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 12, b = 38;
+    int sum = a + b;
+    int product = a * b;
+    cout << "Sum=" << sum << " Product=" << product << endl;
+    return 0;
+}
