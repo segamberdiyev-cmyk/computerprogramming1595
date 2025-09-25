9f661b788bb3168f619ad90ab1a3cd1018f6162e
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 16:45:25 +0500

    Create problem3.cpp

---
+++ problem3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << R"(”I’m standing” on the edge of some crazy cliff. What I have to do, I have to catch everybody\ if they start to go over the cliff—I mean\ \if they’re running and they don’t look where they’re going I have to come out from somewhere and catch them.)" << std::endl;
+    return 0;
+}
