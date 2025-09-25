fa1f62bada00e59ba033886e32aa07bbc375c65f
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 17:12:45 +0500

    Create problen13.cpp

---
+++ problen13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double s;
+    cin >> s;
+    double area = (3 * sqrt(3) / 2) * s * s;
+    cout << "The area of the hexagon is " << area << endl;
+    return 0;
+}
