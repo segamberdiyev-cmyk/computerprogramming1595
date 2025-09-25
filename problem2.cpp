558f5b2cd305ffa92bd1fb752da0b22993ae0cd5
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 16:43:09 +0500

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Sultonali Egamberdiyev" << endl;
+    cout << "240360" << endl;
+    cout << "Oq bulut" << endl;
+    cout << "Naruto" << endl;
+
+    return 0;
+}
