ab82abb1bfa3f0d7bf1ee6e8bb8c6ecf0c37f725
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 17:02:30 +0500

    Create problem7.cpp

---
+++ problem7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x, y;
+    cin >> x >> y;
+    float t = 3 * (x * x + 3) / (y * 0,25 + 3);
+    cout << "t=" << t << endl;
+    return 0;
+}
