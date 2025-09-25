73dc6ec6e60f27a7474cacecf0630a029e695d24
segamberdiyev-cmyk <s.egamberdiyev@newuu.uz>
2025-09-25 17:13:41 +0500

    Create problem14.cpp

---
+++ problem14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 31;
+    num = num + 1;
+    num = num * 3;
+    num = num - 14;
+    num = num / 4;
+    num = num - 1;
+    num = num % 9;
+    cout << num << endl;
+    return 0;
+}
